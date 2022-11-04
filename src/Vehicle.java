d7ec0f4e08e2403a7979bfd5ef93eedb8cbc8ee1
Your Name <alo20172@uvg.edu.gt>
2022-11-04 11:47:15 -0600

    Added Vehicles class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,14 @@
 public class Vehicle {
     private int license_plate;
     private String vehicle_type;
-    
+    private String gas_type;
+    public int getLicensePlate() {
+        return license_plate;
+    }
+    public String getVehicleType() {
+        return vehicle_type;
+    }
+    public String getGasType() {
+        return gas_type;
+    }
 }
